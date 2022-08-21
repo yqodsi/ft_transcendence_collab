d516fb1c305521062afd039a0d0f4ec208d79220
yabdelgh <yassine.abdou.1998@gmail.com>
2022-08-21 20:35:37 +0100

    added chat-module and chat-gateway

--- api/src/chat/chat.module.ts
+++ api/src/chat/chat.module.ts
@@ -1,7 +1,7 @@
 import { Module } from '@nestjs/common';
-import { ChatService } from './chat.service';
+import { ChatGateway } from './chat.gateway';
 
 @Module({
-  providers: [ChatService]
+  providers: [ChatGateway]
 })
 export class ChatModule {}
